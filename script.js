36264f28778de6f3f0eb991a45c415df8ae17a4e
Ricardo Polanco <polancoblancoricardo@gmail.com>
2023-01-26 17:32:09 -0600

    :rocket:

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ menu.addEventListener("click", function(){
 window.onscroll = () => {
     navbar.classList.remove("active");
 }
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        } else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
